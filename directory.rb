ea4f0a1aace9ec3d7e567bc4704589719261521f
nicola <nicolaccarroll@gmail.com>
2021-07-21 12:41:23 +0100

    adding interactive menu

--- directory.rb
+++ directory.rb
@@ -83,25 +83,46 @@ def print_footer(names)
   end
 end
 
-students = input_students
-
-print_header(students)
-print(students)
-print_footer(students)
-
-students_by_cohort = {}
-
-students.each do |student|
-  month = student[:cohort]
-  name = student[:name]
-  if students_by_cohort[month].nil?
-    students_by_cohort[month] = [name]
-  else
-    students_by_cohort[month].push(name)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      # input students
+      students = input_students
+    when "2"
+      # show the students
+      print_header(students)
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
   end
 end
 
-students_by_cohort.each do |cohort, names|
-  puts cohort
-  puts names
-end
+
+interactive_menu
+
+# students_by_cohort = {}
+
+# students.each do |student|
+#   month = student[:cohort]
+#   name = student[:name]
+#   if students_by_cohort[month].nil?
+#     students_by_cohort[month] = [name]
+#   else
+#     students_by_cohort[month].push(name)
+#   end
+# end
+
+# students_by_cohort.each do |cohort, names|
+#   puts cohort
+#   puts names
+# end
